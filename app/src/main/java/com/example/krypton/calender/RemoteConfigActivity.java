2ba5abf43af91c9002c4c5313f9005993cf36a9b
Mohamed-Shabeer-KP <mohammedshabeerkp@gmail.com>
2018-06-25 15:00:45 +0530

    REMOTE CONFIG 0.2

--- app/src/main/java/com/example/krypton/calender/RemoteConfigActivity.java
+++ app/src/main/java/com/example/krypton/calender/RemoteConfigActivity.java
@@ -15,7 +15,7 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class RemoteConfigActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    // Remote Config keys
+   
     private static final String LOADING_PHRASE_CONFIG_KEY = "loading_phrase";
     private static final String WELCOME_MESSAGE_KEY = "welcome_message";
     private static final String WELCOME_MESSAGE_CAPS_KEY = "welcome_message_caps";
@@ -38,28 +38,16 @@ public class RemoteConfigActivity extends AppCompatActivity {
             }
         });
 
-        // Get Remote Config instance.
-        // [START get_remote_config_instance]
-        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-        // [END get_remote_config_instance]
 
-        // Create a Remote Config Setting to enable developer mode, which you can use to increase
-        // the number of fetches available per hour during development. See Best Practices in the
-        // README for more information.
-        // [START enable_dev_mode]
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 .setDeveloperModeEnabled(BuildConfig.DEBUG)
                 .build();
         mFirebaseRemoteConfig.setConfigSettings(configSettings);
-        // [END enable_dev_mode]
 
-        // Set default Remote Config parameter values. An app uses the in-app default values, and
-        // when you need to adjust those defaults, you set an updated value for only the values you
-        // want to change in the Firebase console. See Best Practices in the README for more
-        // information.
-        // [START set_default_values]
         mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
-        // [END set_default_values]
+
 
         fetchWelcome();
     }
@@ -70,18 +58,12 @@ public class RemoteConfigActivity extends AppCompatActivity {
     private void fetchWelcome() {
         mWelcomeTextView.setText(mFirebaseRemoteConfig.getString(LOADING_PHRASE_CONFIG_KEY));
 
-        long cacheExpiration = 3600; // 1 hour in seconds.
-        // If your app is using developer mode, cacheExpiration is set to 0, so each fetch will
-        // retrieve values from the service.
+        long cacheExpiration = 3600;
         if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
             cacheExpiration = 0;
         }
 
-        // [START fetch_config_with_callback]
-        // cacheExpirationSeconds is set to cacheExpiration here, indicating the next fetch request
-        // will use fetch data from the Remote Config service, rather than cached parameter values,
-        // if cached parameter values are more than cacheExpiration seconds old.
-        // See Best Practices in the README for more information.
+
         mFirebaseRemoteConfig.fetch(cacheExpiration)
                 .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
@@ -100,18 +82,16 @@ public class RemoteConfigActivity extends AppCompatActivity {
                         displayWelcomeMessage();
                     }
                 });
-        // [END fetch_config_with_callback]
+
     }
 
     /**
      * Display a welcome message in all caps if welcome_message_caps is set to true. Otherwise,
      * display a welcome message as fetched from welcome_message.
      */
-    // [START display_welcome_message]
+
     private void displayWelcomeMessage() {
-        // [START get_config_values]
         String welcomeMessage = mFirebaseRemoteConfig.getString(WELCOME_MESSAGE_KEY);
-        // [END get_config_values]
         if (mFirebaseRemoteConfig.getBoolean(WELCOME_MESSAGE_CAPS_KEY)) {
             mWelcomeTextView.setAllCaps(true);
         } else {
@@ -119,5 +99,5 @@ public class RemoteConfigActivity extends AppCompatActivity {
         }
         mWelcomeTextView.setText(welcomeMessage);
     }
-    // [END display_welcome_message]
+
 }
