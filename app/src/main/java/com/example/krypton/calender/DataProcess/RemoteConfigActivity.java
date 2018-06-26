9c103456ec6ffffea28edcc4094f43576797082c
Mohamed-Shabeer-KP <mohammedshabeerkp@gmail.com>
2018-06-26 16:28:32 +0530

    SHARED PREFERENCES V0.1

--- app/src/main/java/com/example/krypton/calender/DataProcess/RemoteConfigActivity.java
+++ app/src/main/java/com/example/krypton/calender/DataProcess/RemoteConfigActivity.java
@@ -1,20 +1,19 @@
-package com.example.krypton.calender;
+package com.example.krypton.calender.DataProcess;
 
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.krypton.calender.BuildConfig;
+import com.example.krypton.calender.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class RemoteConfigActivity extends AppCompatActivity {
-
     private static final String LOADING_PHRASE_CONFIG_KEY = "loading_phrase";
     private static final String WELCOME_MESSAGE_KEY = "welcome_message";
     private static final String WELCOME_MESSAGE_CAPS_KEY = "welcome_message_caps";
@@ -29,15 +28,14 @@ public class RemoteConfigActivity extends AppCompatActivity {
 
         mWelcomeTextView = findViewById(R.id.welcomeTextView);
 
-       Button fetchButton = findViewById(R.id.fetchButton);
+      /* Button fetchButton = findViewById(R.id.fetchButton);
        fetchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
         fetchWelcome();
            }
         });
-
-
+       */
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
@@ -51,9 +49,6 @@ public class RemoteConfigActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * Fetch a welcome message from the Remote Config service, and then activate it.
-     */
     private void fetchWelcome() {
         mWelcomeTextView.setText(mFirebaseRemoteConfig.getString(LOADING_PHRASE_CONFIG_KEY));
 
@@ -61,33 +56,21 @@ public class RemoteConfigActivity extends AppCompatActivity {
         if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
             cacheExpiration = 0;
         }
-
-
         mFirebaseRemoteConfig.fetch(cacheExpiration)
                 .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(RemoteConfigActivity.this, "Fetch Succeeded",
-                                    Toast.LENGTH_SHORT).show();
-
-                            // After config data is successfully fetched, it must be activated before newly fetched
-                            // values are returned.
+                            Toast.makeText(RemoteConfigActivity.this, "Fetch Succeeded", Toast.LENGTH_SHORT).show();
                             mFirebaseRemoteConfig.activateFetched();
                         } else {
-                            Toast.makeText(RemoteConfigActivity.this, "Fetch Failed",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RemoteConfigActivity.this, "Fetch Failed", Toast.LENGTH_SHORT).show();
                         }
                         displayWelcomeMessage();
                     }
                 });
 
     }
-
-    /**
-     * Display a welcome message in all caps if welcome_message_caps is set to true. Otherwise,
-     * display a welcome message as fetched from welcome_message.
-     */
 
     private void displayWelcomeMessage() {
         String welcomeMessage = mFirebaseRemoteConfig.getString(WELCOME_MESSAGE_KEY);
@@ -100,3 +83,4 @@ public class RemoteConfigActivity extends AppCompatActivity {
     }
 
 }
+
--- app/src/main/java/com/example/krypton/calender/MainActivity.java
+++ app/src/main/java/com/example/krypton/calender/MainActivity.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.krypton.calender.Calander.ActivityAPI;
 import com.example.krypton.calender.Calander.EventActivity;
 import com.example.krypton.calender.CloudMessaging.CloudMessageActivity;
+import com.example.krypton.calender.DataProcess.RemoteConfigActivity;
+import com.example.krypton.calender.DataProcess.SharedPreferencesJava;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -20,35 +23,19 @@ public class MainActivity extends AppCompatActivity {
     private Button B2;
     private Button B3;
     private Button B4;
+    private Button B5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       // startActivity(new Intent(getApplicationContext(),CloudMessageActivity.class));
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create channel to show notifications.
-            String channelId  = getString(R.string.default_notification_channel_id);
-            String channelName = getString(R.string.default_notification_channel_name);
-            NotificationManager notificationManager =
-                    getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
-                    channelName, NotificationManager.IMPORTANCE_LOW));
-        }
-        if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
-                Object value = getIntent().getExtras().get(key);
-                Log.d(TAG, "Key: " + key + " Value: " + value);
-            }
-        }
-
 
 
         B1= findViewById(R.id.b1);
         B2= findViewById(R.id.b2);
         B3= findViewById(R.id.b3);
         B4= findViewById(R.id.b4);
+        B5= findViewById(R.id.b5);
 
         B1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,7 +64,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), CloudMessageActivity.class));
             }
         });
+       B5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SharedPreferencesJava.class));
+            }
+        });
 
+        //----------------CLOUD MESSAGING----------//
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+          /*
+           if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        */
+        //----------------CLOUD MESSAGING----------//
     }
-
 }
+
