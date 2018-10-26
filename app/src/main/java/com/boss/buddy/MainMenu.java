3a9500534298ee713ce672b1898a3e60820ce9e9
surajsuresh29 <surajcheboy29@gmail,com>
2018-10-26 11:53:37 +0530

    Snack Bar Update

--- app/src/main/java/com/boss/buddy/MainMenu.java
+++ app/src/main/java/com/boss/buddy/MainMenu.java
@@ -13,6 +13,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -29,7 +30,13 @@ import android.widget.Toast;
 
 import com.boss.buddy.CalendarEvent.EventActivity;
 import com.boss.buddy.TimeTable.Class_Time_Table_activity;
+import com.bumptech.glide.Glide;
 import com.google.api.client.util.DateTime;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Date;
 
@@ -37,21 +44,38 @@ import java.util.Date;
 public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     boolean exit = false;
+    String message = "";
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "RestrictedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("notification");
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                message = (String) dataSnapshot.child("message").getValue();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+
+        });
 
         Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab =  findViewById(R.id.fab);
+
+        final FloatingActionButton fab =  findViewById(R.id.fab);
+           // fab.setVisibility(View.VISIBLE);
         fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hey Buddy...! This space will be filled soon.", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, message, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
